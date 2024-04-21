ce74806c59543b07ff78debfb9e14b5842bda507
omithassain28 <115416573+omithassain28@users.noreply.github.com>
2024-04-21 18:25:44 +0600

    Update About.js

--- src/commands/info/About.js
+++ src/commands/info/About.js
@@ -33,46 +33,46 @@ module.exports = class About extends Command {
   async run(client, ctx) {
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
-        .setLabel("Invite WaveMusic")
+        .setLabel("Invite Me")
         .setStyle(ButtonStyle.Link)
         .setURL(
-          `https://discord.com/api/oauth2/authorize?client_id=875635121770889257&permissions=8&scope=bot%20applications.commands`
+          `https://discord.gg/47pCJuCEac`
         ),
       new ButtonBuilder()
         .setLabel("Support Server")
         .setStyle(ButtonStyle.Link)
-        .setURL("https://discord.gg/ns8CTk9J3e")
+        .setURL("https://discord.gg/47pCJuCEac")
     );
     const embed = this.client
       .embed()
       .setAuthor({
-        name: "WaveMusic",
+        name: "Felicità",
         iconURL:
-          "https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png",
+          "https://images-ext-1.discordapp.net/external/dff2nObSuQY8mWBGGMNePLc4G0Ghw7Pxtgq2T4bKuWQ/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/1064447751984791572/6c93d68ac2a30ab765a3c3637eba36e9.png?format=webp&quality=lossless&width=416&height=416",
       })
       .setThumbnail(
-        "https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png"
+        "https://images-ext-1.discordapp.net/external/dff2nObSuQY8mWBGGMNePLc4G0Ghw7Pxtgq2T4bKuWQ/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/1064447751984791572/6c93d68ac2a30ab765a3c3637eba36e9.png?format=webp&quality=lossless&width=416&height=416"
       )
       .setColor(this.client.color.main)
       .addFields([
         {
           name: "Creator",
-          value: "[Blacky#9125](https://github.com/brblacky)",
+          value: "[king5929](https://discord.gg/47pCJuCEac)",
           inline: true,
         },
         {
           name: "Repository",
-          value: "[Here](https://github.com/brblacky/WaveMusic)",
+          value: "[Here](https://discord.gg/47pCJuCEac)",
           inline: true,
         },
         {
           name: "Support",
-          value: "[Here](https://discord.gg/ns8CTk9J3e)",
+          value: "[Here](https://discord.gg/47pCJuCEac)",
           inline: true,
         },
         {
           name: "\u200b",
-          value: `He really wanted to make his first open source project ever for more coding experience. In this project, he was challenged to make a project with less bugs. Hope you enjoy using WaveMusic!`,
+          value: `He really wanted to make his first open source project ever for more coding experience. In this project, he was challenged to make a project with less bugs. Hope you enjoy using Felicità!`,
           inline: true,
         },
       ]);
